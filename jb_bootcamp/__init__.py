7520d4a67fc774ef811c86498ef8532114615777
Catherine Li <cali@scripps.edu>
2023-08-15 14:01:30 -0700

    Initial commit of bootcamp utilities.

---
+++ jb_bootcamp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for utilities for bootcamp."""
+
+from .na_utils import *
+from .bioinfo_dicts import *
+
+__author__ = 'Catherine Li'
+__email__ = 'cali@scripps.edu'
+__version__ = '0.0.1'
